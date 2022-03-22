ad27af15e4759c0b52d926429397c57996f56279
doyochi <nuzululhikmah117@gmail.com>
2022-03-22 20:06:08 +0700

    add fragment layout and binding, also on destroy binding null

--- app/src/main/java/id/hikmah/binar/navigationapp/MainActivity.kt
+++ app/src/main/java/id/hikmah/binar/navigationapp/MainActivity.kt
@@ -2,10 +2,15 @@ package id.hikmah.binar.navigationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.hikmah.binar.navigationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
